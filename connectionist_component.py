806fde57f325604027462a0697d767c4304591a9
dosostenido <80398908+dosostenido@users.noreply.github.com>
2022-11-20 10:30:05 +0100

    Add files via upload

--- connectionist_component.py
+++ connectionist_component.py
@@ -4,13 +4,57 @@
 # ====================================
 import matplotlib.pyplot as plt
 import numpy as np
+from itertools import chain # F new
 
 from keras.models import Sequential
 from keras.layers import SimpleRNN, Dense
 from keras import initializers
 from sklearn.model_selection import train_test_split
 
-from data_encoding import premise_decoder
+#from data_encoding import premise_decoder
+
+
+def _indexing(x, indices):
+    """
+    :param x: array from which indices has to be fetched
+    :param indices: indices to be fetched
+    :return: sub-array from given array and indices
+    """
+    # np array indexing
+    if hasattr(x, 'shape'):
+        return x[indices]
+
+    # list indexing
+    return [x[idx] for idx in indices]
+
+def train_test_split1(*arrays, test_size=0.25, shufffle=True, random_seed=1):
+    """
+    splits array into train and test data.
+    :param arrays: arrays to split in train and test
+    :param test_size: size of test set in range (0,1)
+    :param shufffle: whether to shuffle arrays or not
+    :param random_seed: random seed value
+    :return: return 2*len(arrays) divided into train ans test
+    """
+    # checks
+    assert 0 < test_size < 1
+    assert len(arrays) > 0
+    length = len(arrays[0])
+    for i in arrays:
+        assert len(i) == length
+
+    n_test = int(np.ceil(length*test_size))
+    n_train = length - n_test
+
+    if shufffle:
+        perm = np.random.RandomState(random_seed).permutation(length)
+        test_indices = perm[:n_test]
+        train_indices = perm[n_test:]
+    else:
+        train_indices = np.arange(n_train)
+        test_indices = np.arange(n_train, length)
+
+    return list(chain.from_iterable((_indexing(x, train_indices), _indexing(x, test_indices)) for x in arrays))
 
 # STATISTICS
 # create a log of results (proofs of length 1)
@@ -157,7 +201,7 @@ def fit_model(X, Y, model_type = 'MLP', **options):
         strat = [sum(y) for y in Y] # default option    
 
     # MLP activation function for hidden layers:
-    if '    ' in options.keys():
+    if 'activation_function' in options.keys():
         activation_function = options['activation_function']
     else:
         activation_function = 'relu'
@@ -170,11 +214,13 @@ def fit_model(X, Y, model_type = 'MLP', **options):
 
     # DATA SPLIT    
     val_size = split[1] / (1 - split[2])
+    print(val_size)
     if split[2] == 0:
         X_train = X
         Y_train = Y
     else:
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = split[2], stratify = strat)
+        #X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = split[2], stratify = strat)
+        X_train, X_test, Y_train, Y_test = train_test_split1(X, Y, test_size = split[2])
 
     # MODEL DEFINITION    
     model = Sequential() # instantiation of NN
